395c0b810c4fed941400060cd05642442c56cd7a
Pratik Bassi <35152777+pratikbassi@users.noreply.github.com>
2025-04-21 01:05:13 -0400

    updated settings

--- backend/settings.py
+++ backend/settings.py
@@ -135,3 +135,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
